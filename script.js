1a1eebacaedda481a7a580ff294256a9f4fd5a0f
snaketrail <stefdim0211@gmail.com>
2024-08-07 17:40:57 +0300

    i decided to focus on 1 part of the story first

--- script.js
+++ script.js
@@ -7,31 +7,25 @@ const skills = [];
 const titles = [];
 const karmaPoints = 0;
 const malKarmaPoints = 0;
-
+// ,
+//     effect: () => {
+//       hp -= 20;
+//       alert(`HP decreased by 20. Current HP: ${hp}`);
+//     }
 const scenes = {
   '1': {
     text: "What dragons? Where did you see them, boy? Oh! OH! RUUUUN!",
     options: [
       { text: "Run.", nextScene: '1.1' },
-      { text: "Walk.", nextScene: '1.2' },
-      { text: "Stay.", nextScene: '1.3' },
-      { text: "Try to speak to the dragons.", nextScene: '1.3' }
+      { text: "Walk.", nextScene: '1' },
+      { text: "Stay.", nextScene: '1' },
+      { text: "Try to speak to the dragons.", nextScene: '1' }
     ]
   },
   '1.1': {
     text: "Your run as fast as possible. You don't even look behind to see if the dragosn are still around. Even if they were, you would not choose to reason with a legendary creature.",
     options: [
       { text: "Continue.", nextScene: '1.1.1' }
-    ]
-  },
-  '1.2': {
-    text: "You start walking. Eventually the dragons see you and one lands right next to you. It looks you in the eye and says something in Dragonic.",
-    options: [
-      { text: "No habla ingles.", nextScene: '1.2.1' },
-      { text: "Sorry! I don't speak Dragonic.", nextScene: '1.2.2' },
-      { text: "Do you happen to know Common?", nextScene: '1.2.3' },
-      { text: "Scream like a little girl.", nextScene: '1.2.4' }
-
     ]
   },
   '1.1.1': {
@@ -41,71 +35,38 @@ const scenes = {
       { text: "Check the bushes.", nextScene: '1.1.1.2' }
     ]
   },
-  '1.1.2': {
-    text: "You decided to take a break. You feel relaxed and refreshed.",
-    options: [
-      { text: "Return to work.", nextScene: '1.1.1' },
-      { text: "Extend the break.", nextScene: '1.1.2.1' }
-    ]
-  },
-  
-  '1.2.4': {
-    text: "You let out a little girl scream. The dragon died laughing.",
-    options: [
-      { text: "No habla ingles.", nextScene: '1' },
-      { text: "Sorry! I don't speak Dragonic.", nextScene: '1.2.2' },
-      { text: "Do you happen to know Common?", nextScene: '1.2.3' },
-      { text: "Scream like a little girl.", nextScene: '1.2.4' }
-
-    ],
-    effect: () => {
-      hp -= 20;
-      alert(`HP decreased by 20. Current HP: ${hp}`);
-    }
-  },
-
-  '1.2.1': {
-    text: "You joking said to the dragon. Unfortunetely, they don't appreciate jokes from inferior creatures. The dragon burned you alive with his breath.",
-    options: [
-      { text: "Start again.", nextScene: '1' }
-    ]
-  },
-  '1.2.2': {
-    text: "Ugh! Stupid creatures can't learn one damned tongue. I was asking where is the dungeon of Malagarth?",
-    options: [
-      { text: "F.. YOU!", nextScene: '1.2.2.1' },
-      { text: "Yeah. It is right over the mountain range.", nextScene: '1.2.2.2' },
-      { text: "If you let me ride you, I will show you exactly where it is.", nextScene: '1.2.2.3' },
-      { text: "I thought dragons hated adventurers. Now you want to become one?", nextScene: '1.2.2.4' }
-
-    ]
-  },
-  '1.2.2.1': {
-    text: "It happened so fast that even you don't know how you died.",
-    options: [
-      { text: "Start over.", nextScene: '1' },
-    ]
-  },
   '1.1.1.1': {
     text: "You enter the cave and see that somebody or something turned it into a home. There's a bed, a table, a chair even something that looks like a shower.",
     options: [
-      { text: "Take a shower.", nextScene: '1.1.1.2' },
-      { text: "Sleep in the bed.", nextScene: '1.1.1.2' },
-      { text: "Wait for the dragons to pass.", nextScene: '1.1.1.2' },
-      { text: "Search for anything of value.", nextScene: '1.1.1.2' },
+      { text: "Take a shower.", nextScene: '1.1.1.1.1' },
+      { text: "Sleep in the bed.", nextScene: '1.1.1.1.2' },
+      { text: "Wait for the dragons to pass.", nextScene: '1.1.1.1.3' },
+      { text: "Search for anything of value.", nextScene: '1.1.1.1.4' }
+
+    ]
+  },
+  '1.1.1.1.1': {
+    text: "You tried using the shower but you had no idea how to turn it on. Also, it seemed kind of silly to take a shower in the home of a random stranger, specially if it is in a cave in the forest.",
+    options: [
+      { text: "Sleep in the bed.", nextScene: '1.1.1.1.2' },
+      { text: "Wait for the dragons to pass.", nextScene: '1.1.1.1.3' },
+      { text: "Search for anything of value.", nextScene: '1.1.1.1.4' }
+
+    ]
+  },
+  '1.1.1.1.2': {
+    text: "You tried sleeping in the bed, but to no avail. Your heart is still beating too fast because you ran from the dragons. Maybe you should wait until you calm down.",
+    options: [
+      { text: "Wait a bit more to calm down.", nextScene: '1.1.1.1.2.1' },
+      { text: "Check to see if the dragons are gone.", nextScene: '1.1.1.1.2.2' }
 
     ]
   },
   '1.1.1.2': {
-    text: "A snake bit your face and melted it. You died!",
+    text: "You check the bushes. Nothing is there. Suddently you feel a sharp pain in your neck. You faint.",
     options: [
-      { text: "Start over.", nextScene: '1' },
-    ]
-  },
-  '1.1.2.1': {
-    text: "You extended the break and eventually decided to quit your job. Freedom feels good!",
-    options: [
-      { text: "Start a new adventure.", nextScene: '1' },
+      { text: "Continue.", nextScene: '1.1.1.2.1' }
+
     ]
   }
 };
